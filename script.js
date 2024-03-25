a66665864fc1bb197c638543a1bbf6c50666ddbe
jbui184 <johnnybui184@gmail.com>
2024-03-25 21:54:17 +1100

    buttons working

--- script.js
+++ script.js
@@ -11,3 +11,22 @@
 
 
 /*----- functions -----*/
+
+// window.onload() = function() {
+// //instructions();//
+// startGame()
+
+// }
+
+
+const buttonSelector = document.querySelectorAll(".color-buttons > .color");
+
+buttonSelector.forEach(function(button) {
+    button.addEventListener("click", function() {
+        selectColor(button.id)  })
+})
+
+function selectColor(color){
+   console.log(color)
+}
+
