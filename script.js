4f4ec6ea8d3e51139059d1d2561e09225365e75d
jbui184 <johnnybui184@gmail.com>
2024-04-05 19:58:01 +1100

    fixed colour selection and hint validation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*----- constants -----*/
 const colors = ["red", "blue", "yellow", "green", "orange", "purple"];
 const hintColors = ["grey", "white", "black"];
+
 const board = document.querySelector(".board");
 
 const redButton = document.querySelector("button.red");
@@ -9,50 +10,53 @@ const yellowButton = document.querySelector("button.yellow");
 const greenButton = document.querySelector("button.green");
 const orangeButton = document.querySelector("button.orange");
 const purpleButton = document.querySelector("button.purple");
-
+// boardState[currentRowNumber].hint = getHintColorArray(rowChoiceArray);
+// answer - hint[ "grey","black","black","white"]
 const boardState = [
-  { row_number: 1, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 2, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 3, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 4, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 5, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 6, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 7, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 8, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 9, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { row_number: 10, choice: ["", "", "", ""], hint: ["", "", "", ""] },
-  { computerCode: ["", "", "", ""] },
+  { row_number: 1, choice: [], hint: [] },
+  { row_number: 2, choice: [], hint: [] },
+  { row_number: 3, choice: [], hint: [] },
+  { row_number: 4, choice: [], hint: [] },
+  { row_number: 5, choice: [], hint: [] },
+  { row_number: 6, choice: [], hint: [] },
+  { row_number: 7, choice: [], hint: [] },
+  { row_number: 8, choice: [], hint: [] },
+  { row_number: 9, choice: [], hint: [] },
+  { row_number: 10, choice: [], hint: [] },
 ];
+const answerState = generateSecretCode(colors);
 /*----- state variables -----*/
-let secretCode = generateSecretCode(colors);
-let selectionChoiceRow1 = [];
-let selectionChoiceRow2 = [];
-let selectionChoiceRow3 = [];
-let selectionChoiceRow4 = [];
-let selectionChoiceRow5 = [];
-let selectionChoiceRow6 = [];
-let selectionChoiceRow7 = [];
-let selectionChoiceRow8 = [];
-let selectionChoiceRow9 = [];
-let selectionChoiceRow10 = [];
-let hintRow1 = [];
-let hintRow2 = [];
-let hintRow3 = [];
-let hintRow4 = [];
-let hintRow5 = [];
-let hintRow6 = [];
-let hintRow7 = [];
-let hintRow8 = [];
-let hintRow9 = [];
-let hintRow10 = [];
+
+let currentRowNumber = 0;
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
+// BUTTONS
+redButton.addEventListener("click", function () {
+  addColor("red");
+});
+blueButton.addEventListener("click", function () {
+  addColor("blue");
+});
+yellowButton.addEventListener("click", function () {
+  addColor("yellow");
+});
+greenButton.addEventListener("click", function () {
+  addColor("green");
+});
+orangeButton.addEventListener("click", function () {
+  addColor("orange");
+});
+purpleButton.addEventListener("click", function () {
+  addColor("purple");
+});
 /*----- functions -----*/
 
 function renderBoard(gameBoard) {
   board.innerHTML = "";
+
+  //   HTML for rendering the guesses
   gameBoard.forEach(function (row) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
@@ -60,80 +64,87 @@ function renderBoard(gameBoard) {
     const rowNumberContainer = document.createElement("div");
     const choiceContainer = document.createElement("div");
     const hintContainer = document.createElement("div");
-    const computerCodeContainer = document.createElement("div");
 
     choiceContainer.classList.add("selection");
     hintContainer.classList.add("hint");
-    computerCodeContainer.classList.add("computer-code");
 
     const rowNumberDiv = document.createElement("div");
     rowNumberDiv.textContent = row.row_number;
     rowNumberContainer.append(rowNumberDiv);
 
-    const computerCodeDiv = document.createElement("div");
-    computerCodeDiv.classList.add("computer-code");
-    computerCodeContainer.append(computerCodeDiv);
-
     for (let i = 0; i < 4; i++) {
       const choiceDiv = document.createElement("div");
       choiceDiv.classList.add("choice");
+      const choiceColor = row.choice[i];
+      if (choiceColor) {
+        choiceDiv.classList.add(choiceColor);
+      }
       choiceContainer.append(choiceDiv);
     }
-
     for (let i = 0; i < 4; i++) {
       const hintDiv = document.createElement("div");
       hintDiv.classList.add("pegs");
+      const hintColor = row.hint[i];
+      if (hintColor) {
+        hintDiv.classList.add(hintColor);
+      }
       hintContainer.append(hintDiv);
     }
-
-    rowDiv.append(
-      computerCodeContainer,
-      rowNumberContainer,
-      choiceContainer,
-      hintContainer
-    );
+    rowDiv.append(rowNumberContainer, choiceContainer, hintContainer);
     board.append(rowDiv);
   });
+
+  // HTML for computer code
+  const secretContainer = document.createElement("div");
+  secretContainer.classList.add("computer-code");
+
+  for (let i = 0; i < 4; i++) {
+    const secretCodeDiv = document.createElement("div");
+    secretCodeDiv.classList.add("choice");
+    secretContainer.append(secretCodeDiv);
+  }
+
+  board.append(secretContainer);
 }
 
 function addColor(color) {
-  for (const row of boardState) {
-    for (let i = 0; i < row.guess.length; i++) {
-      if (row.guess[i] === "") {
-        row.guess[i] = color;
-        renderBoard(boardState);
-        return;
-      }
-    }
+  const rowChoiceArray = boardState[currentRowNumber].choice;
+  rowChoiceArray.push(color);
+  if (rowChoiceArray.length === 4) {
+    boardState[currentRowNumber].hint = getHintColorArray(rowChoiceArray);
+    currentRowNumber++;
+    console.log(JSON.stringify(boardState));
+    console.log(answerState);
   }
+  renderBoard(boardState);
+}
+// checking the completed selection/choice and now checking that array aganist the secret code
+function getHintColorArray(array) {
+  let hint = [];
+  array.forEach(function (color, index) {
+    // black = right color, right index
+    if (color === answerState[index]) {
+      hint.push("black");
+      return;
+    }
+    // white = right color in code, wrong index
+    if (answerState.includes(color)) {
+      hint.push("white");
+      return;
+    }
+    // grey = wrong color
+    hint.push("grey");
+  });
+  return hint;
 }
 
 renderBoard(boardState);
-
-redButton.addEventListener("click", function () {
-  addColor(".red");
-});
-blueButton.addEventListener("click", function () {
-  addColor(".blue");
-});
-yellowButton.addEventListener("click", function () {
-  addColor(".yellow");
-});
-greenButton.addEventListener("click", function () {
-  addColor(".green");
-});
-orangeButton.addEventListener("click", function () {
-  addColor(".orange");
-});
-purpleButton.addEventListener("click", function () {
-  addColor(".purple");
-});
 
 // initalisation
 window.onload = function () {
   // instructions();//
   // startGame()
-  console.log("--->", secretCode);
+  console.log("--->", answerState);
 };
 
 // initalising the game/secret code
@@ -167,174 +178,12 @@ function handleRestart() {
 // }
 
 // clicking the colour buttons returns the colour
-const buttonSelector = document.querySelectorAll(".color-buttons button");
+// const buttonSelector = document.querySelectorAll(".color-buttons button");
 
-buttonSelector.forEach(function (button) {
-  button.addEventListener("click", function () {
-    selectColor(button.id);
-  });
-});
+// buttonSelector.forEach(function (button) {
+//   button.addEventListener("click", function () {
+//     selectColor(button.id);
+//   });
+// });
 
 // clicking the color buttons fills the arrays
-
-// function selectColor(color) {
-//   if (selectionChoiceRow1.length < 4) {
-//     selectionChoiceRow1.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + selectionChoiceRow1.length
-//     );
-//     currentTile.classList.add(color);
-//     console.log("____>", "#guess-" + selectionChoiceRow1.length);
-//   } else if (selectionChoiceRow2.length < 4) {
-//     selectionChoiceRow2.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (4 + selectionChoiceRow2.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow3.length < 4) {
-//     selectionChoiceRow3.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (8 + selectionChoiceRow3.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow4.length < 4) {
-//     selectionChoiceRow4.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (12 + selectionChoiceRow4.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow5.length < 4) {
-//     selectionChoiceRow5.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (16 + selectionChoiceRow5.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow6.length < 4) {
-//     selectionChoiceRow6.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (20 + selectionChoiceRow6.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow7.length < 4) {
-//     selectionChoiceRow7.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (24 + selectionChoiceRow7.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow8.length < 4) {
-//     selectionChoiceRow8.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (28 + selectionChoiceRow8.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow9.length < 4) {
-//     selectionChoiceRow9.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (32 + selectionChoiceRow9.length)
-//     );
-//     currentTile.classList.add(color);
-//   } else if (selectionChoiceRow10.length < 4) {
-//     selectionChoiceRow10.push(color);
-//     const currentTile = document.querySelector(
-//       "#guess-" + (36 + selectionChoiceRow10.length)
-//     );
-//     currentTile.classList.add(color);
-//   }
-//   if (selectionChoiceRow1.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow2.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow3.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow4.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow5.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow6.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow7.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow8.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow9.length === 4) {
-//     hints(hintColors);
-//   }
-//   if (selectionChoiceRow10.length === 4) {
-//     hints(hintColors);
-//   }
-// }
-
-// check hints
-// split up functions varible that looked for event, in the function - what was clicked - put a colour on the board, when four colours on the board
-// 3 call back functions should be issues, check sequence, check color, check result
-// change variable
-
-// const spots = document.querySelector("#hint-");
-
-// const hints = function (event) {
-//   if (spots === 4) {
-//     checkSequence();
-//     checkColor();
-//     checkResult();
-//   }
-// };
-
-// function checkColor(hintColors) {
-//   if (spots.styles.backgroundColor === secretCode.styles.backgroundColor) {
-//     hintRow1.push(hintColors);
-//   }
-// }
-
-// function checkSequence() {
-// 	boardPosition[indexCounter].map(function(row1,index){
-// if(row1.style.backgroundColor === secretCode[index]){
-// 	resultSpot[indexCounter][index].style.backgroundColor;
-// }
-// 	})
-// }
-
-// function hints() {
-//   if (selectionChoiceRow1.length === 4) {
-//     for (let i = 1; i <= selectionChoiceRow1.length; i++) {
-//       const currentHint = document.getElementById("hint-" + i);
-//       console.log(
-//         getComputedStyle(currentHint).backgroundColor,
-//         "current Hint"
-//       );
-//       if ("guess-" + i === secretCode) {
-//         // console.log(currentHint, "--1--> current Hint");
-//         // console.log(secretCode, "--------> Secret code");
-//         currentHint.classList.add("black");
-//       } else {
-//         // console.log(currentHint, "-- 2 --- >current Hint");
-//         currentHint.classList.add("white");
-//         console.log(
-//           getComputedStyle(currentHint).backgroundColor,
-//           "current Hint"
-//         );
-//       }
-//     }
-//   }
-
-//   if (currentHint.id === ) {
-//     selectionChoiceRow1.forEach(function (color, index) {
-//       if (currentHint[i] === secretCode[i]) {
-//         currentHint.classList.add("black");
-//       } else {
-//         currentHint.classList.add("white");
-//       }
-//     });
-//   }
-//   if (currentHint.id === "hint-3") {
-//     currentHint.classList.add("black");
-//   }
-//   if (currentHint.id === "hint-4") {
-//     currentHint.classList.add("black");
